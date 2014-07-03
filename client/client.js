662527a5b602e36203ac16fa0994b11ab86aa185
oldhobo <marklee.lhs@gmail.com>
2014-07-03 10:34:38 +0800

    Update profile page and topic item display

--- client/client.js
+++ client/client.js
@@ -3,5 +3,5 @@ Meteor.startup(function() {
 	Session.set("showingReplies");
 	Session.setDefault("topicsLimit", 10);
 	Session.setDefault("profileCommentsLimit", 20);
-	Session.setDefault("currentTab", "topicsTab");
+	Session.setDefault("currentTab", "followersTab");
 });
