fc19ae36504c04ee2c80d6b5b44f8b57235fd921
oldhobo <marklee.lhs@gmail.com>
2014-09-11 13:51:45 -0700

    Upgrade to Meteor v0.9.1, fix navbar on large screens

--- client/rendered.js
+++
@@ -1,8 +0,0 @@
-// Template.topic.rendered = function() {
-// 	var hash = document.location.hash.substring(1);
-// 	if (hash) {
-// 	  Meteor.setTimeout(function() {
-// 	  	scrollTo(hash)
-// 	  }, 400);
-// 	}
-// };
