a2cecb53e5b4395ff9367ddc102790f367369b52
oldhobo <marklee.lhs@gmail.com>
2014-09-11 17:56:02 -0700

    Simplify profileUsers publication, move validEmail to global functions

--- server/server.js
+++ server/server.js
@@ -1,8 +1,3 @@
-function validEmail(email) {
-	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(email); 
-}
-
 Accounts.onCreateUser(function(options, user) {
 	user.profile = options.profile;
 	user.activity = options.activity;
