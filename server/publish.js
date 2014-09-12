350a812352f1726a0e839e6a380f067fb374ba7f
oldhobo <marklee.lhs@gmail.com>
2014-09-11 20:53:45 -0700

    Fix issue with reloading topic page by putting allTopics sub in autorun

--- server/publish.js
+++ server/publish.js
@@ -5,20 +5,21 @@ Meteor.publish("profileUser", function(username) { //publish specific user (prof
 	return Meteor.users.find({"username": username}, {fields: {"username": 1, "activity": 1}}); //if username is the current user, meteor will not publish duplicate info
 });
 Meteor.publish("profileUsers", function(username) { //usernames of all the other users in the profile, such as followers/following. separate publish because meteor doesn't allow publishing two cursors from same collection
-	var user = Meteor.users.findOne({"username": username});
-	return Meteor.users.find({"_id": {$in: _.union(user.activity.followers, user.activity.following)}}, {fields: {"username": 1}}); //can't combine this with the return value in publish "profileUser", since the fields we are publishing are different
-});
-Meteor.publish("allTopics", function(limit) { //publish list of topics sorted by date
-	if (limit > TopicsModel.find().count()) {
-		limit = 0;
-	}
-	return TopicsModel.find({}, {limit: limit, sort: {"date": -1}});
+	var user = Meteor.users.findOne({"username": username}),
+			follows = _.union(user.activity.followers, user.activity.following);
+	return Meteor.users.find({"_id": {$in: follows}}, {fields: {"username": 1}}); //can't combine this with the return value in publish "profileUser", since the fields we are publishing are different
 });
 Meteor.publish("topicUsers", function(topic) { 
 	return Meteor.users.find({"_id": this.userId}, {fields: {"activity.liked": 1, "activity.followingTopics": 1}});
 });
 Meteor.publish("topicComments", function(topic) { //publish by topic. sort by likes
 	return CommentsModel.find({"topic": topic}, {sort: {"likes": -1}}); //perhaps limit? display comments currently in the session of shown replies?
+});
+Meteor.publish("allTopics", function(limit) { //publish list of topics sorted by date
+	if (limit > TopicsModel.find().count()) {
+		limit = 0;
+	}
+	return TopicsModel.find({}, {limit: limit, sort: {"date": -1}});
 });
 // Meteor.publish("profileCommentsByUser", function(user, limit) {
 // 	return CommentsModel.find({"owner": user}, {limit: limit, fields: {"_id": 1, "content": 1, "topic": 1}});
