cf86867741de96479191a06a1d4e578bbc95dd49
oldhobo <marklee.lhs@gmail.com>
2014-09-11 17:56:57 -0700

    Restore original location of validEmail

--- client/functions.js
+++ client/functions.js
@@ -16,11 +16,6 @@ var tagOrComment = new RegExp(
   + ')>',
   'gi');
 
-validEmail = function(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email); 
-}
-
 removeTags = function(html) {
   var oldHtml;
   do {
