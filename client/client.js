bb1cb7730922a839f284573c82f133d2ccf7a9cd
markblee <mmaarrkklleeee@gmail.com>
2014-09-15 21:43:50 -0700

    Use session amplify to persist showing replies data

--- client/client.js
+++ client/client.js
@@ -1,6 +1,6 @@
 Meteor.startup(function() {
 	Session.set("currentTopic");
-	Session.set("showingReplies");
+	SessionAmplify.set("showingReplies", []);
 	Session.setDefault("topicsLimit", 10);
 	Session.setDefault("profileCommentsLimit", 20);
 	Session.setDefault("currentTab", "followersTab");
