edf0cdebffefb077ca9848255277e0470f8c3ac9
markblee <mmaarrkklleeee@gmail.com>
2014-09-15 22:08:27 -0700

    Reset showing replies session var when loading topic route

--- client/routes.js
+++ client/routes.js
@@ -6,31 +6,31 @@ Router.configure({
   notFoundTemplate: "notFound"
 });
 
-Router.onBeforeAction(function() {
+Router.onBeforeAction(function () {
   if(!Meteor.loggingIn() && !Meteor.user()) {
     this.redirect("signup");
   }
 }, {except: ["signup", "login"]}); //forgot password page
 
-Router.onBeforeAction(function() {
+Router.onBeforeAction(function () {
   if(Meteor.user()) {
     this.redirect("home");
   }
 }, {only: ["signup", "login"]});
 
-Router.onAfterAction(function() {
+Router.onAfterAction(function () {
   if(this.ready()) {
     var hash = window.location.hash.substring(1);
 
     if(hash) {
-      setTimeout(function() {
+      setTimeout(function () {
         scrollToId(hash);
       }, 0);
     }  
   }
 });
 
-Deps.autorun(function() {
+Deps.autorun(function () {
   Meteor.subscribe("allTopics", Session.get("topicsLimit"));  
 });
 
@@ -39,7 +39,7 @@ Meteor.subscribe("currentUser");
 Router.map(function() {
 	this.route("home", { 
 		path: "/",
-    waitOn: function() {
+    waitOn: function () {
       return Meteor.subscribe("allTopics", Session.get("topicsLimit"));
     }
   });
@@ -47,11 +47,11 @@ Router.map(function() {
   this.route("login", {yieldTemplates: {}}); //don't yield nav
   this.route("profile", {
     path: "/:username",
-    waitOn: function() {
+    waitOn: function () {
       var user = this.params.username;
       return [Meteor.subscribe("profileUser", user), Meteor.subscribe("profileUsers", user), Meteor.subscribe("profileComments", user), Meteor.subscribe("profileTopics", user)];
     },
-    data: function() {
+    data: function () {
       Session.set("currentTab", "followersTab");
       return {
         user: Meteor.users.findOne({"username": this.params.username})
@@ -60,11 +60,11 @@ Router.map(function() {
   });
   this.route("topic", { 
   	path: "/topics/:_id",
-  	waitOn: function() {
+  	waitOn: function () {
   		return [Meteor.subscribe("topicComments", this.params._id), Meteor.subscribe("topicUsers", this.params._id)];
   	},
-  	data: function() {
-      Session.set("showingReplies", []);
+  	data: function () {
+      SessionAmplify.set("showingReplies", []);
   		Session.set("currentTopic", this.params._id);
 			return {
         topic: TopicsModel.findOne({"_id": this.params._id})
