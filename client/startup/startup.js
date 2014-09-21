735c76e01b956cd4948f41913e60f003e089ce65
markblee <mmaarrkklleeee@gmail.com>
2014-09-20 22:22:47 -0700

    Refactor toggleReplies and add comments

--- client/startup/startup.js
+++ client/startup/startup.js
@@ -1,7 +1,7 @@
 Meteor.startup(function() {
 	Session.set("currentTopic");
-	SessionAmplify.set("showingReplies", []);
 	Session.setDefault("topicsLimit", 10);
 	Session.setDefault("profileCommentsLimit", 20);
 	Session.setDefault("currentTab", "followersTab");
+  SessionAmplify.setDefault("showingReplies", []);
 });
