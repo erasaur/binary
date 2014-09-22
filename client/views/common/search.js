ec969be9b274d0b81d975c884314aec1d1d8ad57
markblee <mmaarrkklleeee@gmail.com>
2014-09-21 18:17:24 -0700

    Animate search results and home template with velocity

--- client/views/common/search.js
+++ client/views/common/search.js
@@ -1,7 +1,25 @@
+var searching = false;
+var searchDeps = new Deps.Dependency();
+
 Template.searchInput.indexes = ["topics", "users"];
+Template.searchInput.searching = function () {
+  searchDeps.depend();
+  return searching;  
+}
+Template.home.searching = function () {
+  searchDeps.depend();
+  return searching;
+}
+
 Template.searchInput.events({
   "focus #search-input": function (event, template) {
-    //searching = true, show results
+    searching = true;
+    searchDeps.changed();
+  },
+  "click #search-cancel": function (event, template) {
+    $("#search-input").val('').blur();
+    searching = false;
+    searchDeps.changed();
   }
 });
 
