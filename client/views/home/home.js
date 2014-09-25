fcc021a6b04a5c416217621a6927ed9ef3f450db
markblee <mmaarrkklleeee@gmail.com>
2014-09-24 19:06:40 -0700

    Move create topic event to separate newTopic file

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -6,18 +6,3 @@ Template.home.helpers({
 		return !(TopicsModel.find().count() < Session.get("topicsLimit"));
 	}
 });
-
-Template.home.events({
-	"submit #create-topic-form": function(event, template) {
-		event.preventDefault();
-		var title = template.find("#create-title").value;
-
-		Meteor.call("newTopic", title, Meteor.userId(), Meteor.user().username, function(error, result) {
-			if(error) {
-				alert(formatError(error));
-			} else {
-				$("#create-title").val("");
-			}
-		});
-	}
-});
---
+++ client/views/topics/newTopic.js
@@ -0,0 +1,13 @@
+Template.newTopic.events({
+  "submit #create-topic-form": function(event, template) {
+    event.preventDefault();
+    var title = template.find("#create-title").value;
+
+    Meteor.call("newTopic", title, Meteor.userId(), Meteor.user().username, function(error, result) {
+      if(error)
+        alert(formatError(error));
+      else
+        $("#create-title").val("");
+    });
+  }
+});
