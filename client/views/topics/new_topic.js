cd2e0eb6ab5d5eba252203c17346e00874e1fc09
markblee <mmaarrkklleeee@gmail.com>
2014-09-25 22:05:16 -0700

    Rename models folder to collections, switch to underscore naming for files

--- client/views/topics/new_topic.js
+++ client/views/topics/new_topic.js
@@ -2,12 +2,15 @@ Template.newTopic.events({
   "submit #create-topic-form": function(event, template) {
     event.preventDefault();
     var title = template.find("#create-title").value;
+    // var description = template.find("#create-description").value;
 
     Meteor.call("newTopic", title, Meteor.userId(), Meteor.user().username, function(error, result) {
       if(error)
         alert(formatError(error));
-      else
+      else {
         $("#create-title").val("");
+        // $("#create-description").val("");
+      }
     });
   }
 });
