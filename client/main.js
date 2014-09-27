a66bff724d22b4c98bd7225b9ab23ccc606b4d60
markblee <mmaarrkklleeee@gmail.com>
2014-09-27 12:57:54 -0700

    Move sessions to client main.js, reorganize client folder

---
+++ client/main.js
@@ -0,0 +1,5 @@
+Session.set("currentTopic");
+Session.setDefault("topicsLimit", 10);
+Session.setDefault("profileCommentsLimit", 20);
+Session.setDefault("currentTab", "followersTab");
+SessionAmplify.setDefault("showingReplies", []);
