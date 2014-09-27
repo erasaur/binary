f9bfc3c61c9c435d5d75add0c0495f698a47fe2c
markblee <mmaarrkklleeee@gmail.com>
2014-09-27 12:25:58 -0700

    Add schema to comments, move helper functions to lib, use sanitize html to allow some tags

--- collections/comments.js
+++ collections/comments.js
@@ -1,1 +1,70 @@
+CommentSchema = new SimpleSchema({
+  _id: {
+    type: String,
+    optional: true
+  },
+  content: {
+    type: String
+  },
+  date: {
+    type: Date
+  },
+  likes: { //scoring system ?
+    type: Number,
+    min: 0,
+    optional: true
+  },
+  owner: {
+    type: String
+  },
+  replies: {
+    type: [String]
+  },
+  replyTo: {
+    type: String,
+    optional: true
+  },
+  side: {
+    type: String
+  },
+  topic: {
+    type: String
+  }
+});
+
 Comments = new Mongo.Collection("comments");
+Comments.attachSchema(CommentSchema);
+
+// Comments.allow
+// Comments.deny
+
+Comments.before.insert(function (userId, doc) {
+  if (Meteor.isServer)
+    doc.content = sanitize(doc.content);
+});
+
+Comments.before.update(function (userId, doc, fieldNames, modifier, options) {
+  if (Meteor.isServer && modifier.$set && modifier.$set.content) {
+    modifier.$set = modifier.$set || {};
+    modifier.$set.content = sanitize(modifier.$set.content);
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
