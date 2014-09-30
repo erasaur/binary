78db25b4b70f50203031a0d80806b6fc644450a3
markblee <mmaarrkklleeee@gmail.com>
2014-09-30 12:38:53 -0700

    Implement users tab of profile

--- client/views/profile/profile_users.js
+++ client/views/profile/profile_users.js
@@ -1,8 +1,44 @@
+var userCategory = "Followers";
+var userDeps = new Deps.Dependency();
+
 Template.profileUsers.helpers({
-  following: function () {
-    return this.activity && Meteor.users.find({"_id": {$in: this.activity.following}});
+  userCategory: function () {
+    userDeps.depend();
+    return userCategory;
   },
-  followers: function () {
-    return this.activity && Meteor.users.find({"_id": {$in: this.activity.followers}});
+  users: function () {
+    userDeps.depend();
+    
+    if (this.activity) {
+      return userCategory === "Followers" ? 
+        Meteor.users.find({"_id": {$in: this.activity.followers}}) : 
+        Meteor.users.find({"_id": {$in: this.activity.following}});
+    }
+    return [];
   }
 });
+
+Template.profileUsers.events({
+  "click li[role='presentation']": function (event, template) {
+    userCategory = event.target.text;
+    userDeps.changed();
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
