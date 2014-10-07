94bc22f7d44fcb728c787c9736b6e43944f29b6b
markblee <mmaarrkklleeee@gmail.com>
2014-10-07 11:58:50 -0700

    Separate user server methods, define notification preferences schema, implement settings editing and toggling

--- server/server.js
+++ server/server.js
@@ -1,75 +1,4 @@
-Accounts.onCreateUser(function (options, user) {
-  var userProperties = {
-    profile: options.profile || {},
-    stats: {
-    	reputation: 0,
-	    topicCount: 0,
-	    commentCount: 0,
-	    // isInvited: false,
-	    // invitedCount: 0,
-    },
-    activity: { // activity involving other users/collections
-    	upvotedComments: [],
-			followers: [], 
-			followingUsers: [], 
-			followingTopics: [], 
-			discussedTopics: []
-    },
-    notifications: {
-    	replies: [], // replies to our comments
-    	comments: [], // comments in our topics
-    	followers: [],
-    	followingUsers: { // users we are following
-    		comments: [], // new comments
-    		topics: [] // new topics
-    	}, 
-    	followingTopics: { // topics we are following
-    		comments: []
-    	}
-    }
-  };
-  user = _.extend(user, userProperties);
-
-  if (options.email)
-    user.profile.email = options.email;
-  
-  // set notifications default preferences
-  user.profile.notifications = {
-  	enabled: true,
-    followingUsers: { // users we are following
-    	comments: true, // new comments
-    	topics: true // new topics
-    },
-    followingTopics: { // topics we are following
-    	comments: true // new comments
-    },
-    replies: true, // replies to our comments
-    comments: true, // comments in our topics
-    followers: true // new followers
-  };
-
-  return user;
-});
-
 Meteor.methods({
-	newUser: function (username, email, password) {		
-		var error;
-		
-		if(password.length < 6) {
-			error = "Your password must be at least 6 characters long."; 
-		}
-
-		if(!!error)
-			throw new Meteor.Error(403, error);
-		else {
-			Accounts.createUser({
-				"username": username, 
-				"email": email, 
-				"password": password
-			});
-			return "Success! Your account '" + username + "' has been created.";
-		}
-	},
 	newTopic: function (topic) {
 		var title = topic.title;
 		var description = topic.description;
@@ -80,7 +9,7 @@ Meteor.methods({
 
 		// check if title already exists
 		else {
-			var topicWithTitle = Topics.find({"title": title});
+			var topicWithTitle = Topics.find({ "title": title });
 
 			if (typeof topicWithTitle !== "undefined")
 				throw new Meteor.Error(603, "Sorry, there is already a topic with that title.", topicWithTitle._id);
@@ -115,136 +44,64 @@ Meteor.methods({
 
 		var topicId = Topics.insert(properties);
 
+		Meteor.users.update(userId, { $inc: { "stats.topicsCount": 1 } });
 		Meteor.call("newNotification", "newTopic", userId, { "topicId": topicId, "topicTitle": title });
 
 		return topicId;
 	},
 	newComment: function(userId, topicId, content, side, replyTo, replyToUser) {
 		if (content) {
-			Meteor.users.update(userId, {$addToSet: {"activity.discussedTopics": topicId}});
-			var commentId = Comments.insert({"userId": userId, 
-																			 "topicId": topicId, 
-																			 "createdAt": new Date(), 
-																			 "content": content, 
-																			 "side": side, 
-																			 "upvotes": 0, 
-																			 "replyTo": replyTo, 
-																			 "replies": []
-																			});
-			if (replyTo)
+			Meteor.users.update(userId, { $addToSet: { "activity.discussedTopics": topicId } });
+
+			var properties = {
+				userId: userId,
+				topicId: topicId,
+				createdAt: new Date(),
+				content: content,
+				side: side,
+				upvotes: 0,
+				replyTo: replyTo,
+				replies: []
+			};
+
+			var commentId = Comments.insert(properties);
+
+			if (!!replyTo)
 				Comments.update(replyTo, { $addToSet: { "replies": commentId } });
 
+			Meteor.users.update(userId, { $inc: { "stats.commentsCount": 1 } });
 			Meteor.call("newNotification", "newComment", userId, 
-									{"replyTo": replyToUser || "", "commentId": commentId, "topicId": topicId});
+									{ "replyTo": replyToUser || "", "commentId": commentId, "topicId": topicId });
 
 			return commentId;
-		} else {
+		} else
 			throw new Meteor.Error(403, "Sorry, you can't make a comment with no content.");
-		}
-	},
-	newNotification: function(type, userId, options) {
-		// var	name = getDisplayNameById(userId);
-
-		// if (type === "newComment") {
-		// 	var topicTitle = Topics.findOne(options.topicId).title;
-		// 	var	notobj = {"url": "/topics/" + options.topicId + "#" + options.commentId, 
-		// 								"message": name + " replied to your comment in '" + topicTitle + "'", 
-		// 								"read": false};
-		// 	var	notif;
-
-		// 	//"user replied to your comment"
-		// 	if (options.replyTo && options.replyTo !== userId) { //if reply to self, no notification
-		// 		notif = Meteor.users.findOne(options.replyTo).notifications;
-
-		// 		if (notif.commentReply.length >= 5) {
-		// 			Meteor.users.update(options.replyTo, 
-		// 												 {$pop: {"notifications.commentReply": -1}}); //pop the last item of array
-		// 		}
-
-		// 		Meteor.users.update(options.replyTo, 
-		// 											 {$addToSet: {"notifications.commentReply": notobj}});
-		// 	}
-
-		// 	//"user has posted a new comment in topic"
-		// 	var followers = Meteor.users.findOne(userId).activity.followers; 
-		// 	notobj.message = name + " posted a new comment in '" + topicTitle + "'";
-
-		// 	//notify poster's followers of his new comment
-		// 	_.each(followers, function (id) {
-		// 		if(id !== options.replyTo) { //if it's not the replyTo user (whom we should've already notified)
-		// 			Meteor.users.update(id, {$addToSet: {"notifications.followingUser": notobj}});
-		// 		}
-		// 	});
-
-		// 	var topicFollowers = [];
-		// 	Meteor.users.find({"activity.followingTopics": options.topicId}).forEach(function (user) {
-		// 		if(user._id !== options.replyTo && user._id !== userId) {
-		// 			topicFollowers.push(user._id);
-		// 		}
-		// 	});
-
-		// 	notobj.url = "/topics/" + options.topicId;
-		// 	notobj.message = "A new comment was posted in '" + topicTitle + "'";
-			
-		// 	// notify people who are following the topic in which there is the new comment, 
-		// 	// but not if they have already been notified via the user whom they are following
-		// 	_.each(_.difference(topicFollowers, followers), function (id) {
-		// 		// "a new comment has been posted in topic"
-		// 		var currUrl = "/topics/" + options.topicId; //url for this notification
-
-		// 		// check if a notification entry already has this url
-		// 		notif = _.find(Meteor.users.findOne(id).notifications.followingTopic, function (obj) { 
-		// 			return obj.url === currUrl; 
-		// 		}); 
-
-		// 		//if this topic already has more than one notification entry with the url, combine them all into one with a "count" 
-		// 		if(notif) { //if this topic already has one entry
-		// 			if(notif.count) {
-		// 				Meteor.users.update({"_id": id, "notifications.followingTopic": {$elemMatch: {"url": currUrl}}}, 
-		// 													  {$inc: {"notifications.followingTopic.$.count": 1}});
-		// 			} else {
-		// 				notobj.count = 2;
-		// 				notobj.message = " new comments were posted in '" + topicTitle + "'";
-		// 				Meteor.users.update(id, {$pull: {"notifications.followingTopic": {"url": currUrl}}});
-		// 				Meteor.users.update(id, {$push: {"notifications.followingTopic": notobj}});	
-		// 			}
-		// 		} 
-		// 		//"count" is still low, so just add it as usual, as a normal notification entry
-		// 		else
-		// 			Meteor.users.update(id, {$addToSet: {"notifications.followingTopic": notobj}});
-		// 	});
-		// } else if (type === "newTopic") {
-		// 	_.each(Meteor.users.findOne(userId).activity.followers, function (id) {
-		// 		Meteor.users.update(id, {$addToSet: {"notifications.followingUser": 
-		// 			{"url": "/topics/" + options.topicId, "message": name + " created the new topic '" + options.topicTitle + "'"}}});
-		// 	});
-		// }
 	},
 	newFollower: function (userId, following) {
-		Meteor.users.update(userId, {$addToSet: {"activity.followingUsers": following}});
-		Meteor.users.update(following, {$addToSet: {"activity.followers": userId}});
+		Meteor.users.update(userId, { $addToSet: { "activity.followingUsers": following } });
+		Meteor.users.update(following, { $addToSet: { "activity.followers": userId } });
 	},
 	removeFollower: function (userId, following) {
-		Meteor.users.update(userId, {$pull: {"activity.followingUsers": following}});
-		Meteor.users.update(following, {$pull: {"activity.followers": userId}});
+		Meteor.users.update(userId, { $pull: { "activity.followingUsers": following } });
+		Meteor.users.update(following, { $pull: { "activity.followers": userId } });
 	},
 	upvoteComment: function (userId, commentId, commentOwner) {
-		Comments.update(commentId, {$inc: {"upvotes": 1}});
-		Meteor.users.update(commentOwner, {$inc: {"reputation": 1}});
-		Meteor.users.update(userId, {$addToSet: {"activity.upvotedComments": commentId}});
+		Comments.update(commentId, { $inc: { "upvotes": 1 } });
+		Meteor.users.update(commentOwner, { $inc: { "reputation": 1 } });
+		Meteor.users.update(userId, { $addToSet: { "activity.upvotedComments": commentId } });
 	},
 	downvoteComment: function (userId, commentId, commentOwner) {
-		Comments.update(commentId, {$inc: {"upvotes": -1}});
-		Meteor.users.update(commentOwner, {$inc: {"reputation": -1}});	
-		Meteor.users.update(userId, {$pull: {"activity.upvotedComments": commentId}});
+		Comments.update(commentId, { $inc: { "upvotes": -1 } });
+		Meteor.users.update(commentOwner, { $inc: {"reputation": -1 } });	
+		Meteor.users.update(userId, { $pull: { "activity.upvotedComments": commentId } });
 	},
 	followTopic: function (userId, topicId) {
-		Topics.update(topicId, {$addToSet: {"followers": userId}});
-		Meteor.users.update(userId, {$addToSet: {"activity.followingTopics": topicId}});
+		Topics.update(topicId, { $addToSet: { "followers": userId } });
+		Meteor.users.update(userId, { $addToSet: { "activity.followingTopics": topicId } });
 	},
 	unfollowTopic: function (userId, topicId) {
-		Topics.update(topicId, {$pull: {"followers": userId}});
-		Meteor.users.update(userId, {$pull: {"activity.followingTopics": topicId}});
+		Topics.update(topicId, { $pull: { "followers": userId } });
+		Meteor.users.update(userId, { $pull: { "activity.followingTopics": topicId } });
 	}
 });
 
---
+++ server/users.js
@@ -0,0 +1,184 @@
+Accounts.onCreateUser(function (options, user) {
+  var userProperties = {
+    profile: options.profile || {},
+    stats: {
+      reputation: 0,
+      topicsCount: 0,
+      commentsCount: 0,
+      // isInvited: false,
+      // invitedCount: 0,
+    },
+    activity: { // activity involving other users/collections
+      upvotedComments: [],
+      followers: [], 
+      followingUsers: [], 
+      followingTopics: [], 
+      discussedTopics: []
+    },
+    notifications: {
+      replies: [], // replies to our comments
+      comments: [], // comments in our topics
+      followers: [],
+      followingUsers: { // users we are following
+        comments: [], // new comments
+        topics: [] // new topics
+      }, 
+      followingTopics: { // topics we are following
+        comments: []
+      }
+    }
+  };
+  user = _.extend(user, userProperties);
+
+  if (options.email)
+    user.profile.email = options.email;
+  
+  // set notifications default preferences
+  user.profile.notifications = {
+    enabled: true,
+    followingUsers: { // users we are following
+      comments: true, // new comments
+      topics: true // new topics
+    },
+    followingTopics: { // topics we are following
+      comments: true // new comments
+    },
+    replies: true, // replies to our comments
+    comments: true, // comments in our topics
+    followers: true // new followers
+  };
+
+  return user;
+});
+
+Meteor.methods({
+  newUser: function (username, email, password) {   
+    var error;
+    
+    if (password.length < 6)
+      error = "Your password must be at least 6 characters long.";
+
+    if (!!error)
+      throw new Meteor.Error(403, error);
+    else {
+      Accounts.createUser({
+        "username": username, 
+        "email": email, 
+        "password": password,
+        "profile": {
+          "name": name
+        }
+      });
+      return "Success! Your account '" + username + "' has been created.";
+    }
+  },
+  changeName: function (newName) {
+    Meteor.users.update(Meteor.userId(), { $set: { 'profile.name': newName } });
+  },
+  changeBio: function (newBio) {
+    Meteor.users.update(Meteor.userId(), { $set: { 'profile.bio': newBio } });
+  },
+  changeEmail: function (newEmail) {
+    Meteor.users.update(Meteor.userId(), { $set: { 'emails': [{ 'address': newEmail }] } });
+  },
+  changePreferences: function (newPreferences) {
+    Meteor.users.update(Meteor.userId(), { $set: newPreferences });
+  },
+  newNotification: function(type, userId, options) {
+    // var  name = getDisplayNameById(userId);
+
+    // if (type === "newComment") {
+    //  var topicTitle = Topics.findOne(options.topicId).title;
+    //  var notobj = {"url": "/topics/" + options.topicId + "#" + options.commentId, 
+    //                "message": name + " replied to your comment in '" + topicTitle + "'", 
+    //                "read": false};
+    //  var notif;
+
+    //  //"user replied to your comment"
+    //  if (options.replyTo && options.replyTo !== userId) { //if reply to self, no notification
+    //    notif = Meteor.users.findOne(options.replyTo).notifications;
+
+    //    if (notif.commentReply.length >= 5) {
+    //      Meteor.users.update(options.replyTo, 
+    //                         {$pop: {"notifications.commentReply": -1}}); //pop the last item of array
+    //    }
+
+    //    Meteor.users.update(options.replyTo, 
+    //                       {$addToSet: {"notifications.commentReply": notobj}});
+    //  }
+
+    //  //"user has posted a new comment in topic"
+    //  var followers = Meteor.users.findOne(userId).activity.followers; 
+    //  notobj.message = name + " posted a new comment in '" + topicTitle + "'";
+
+    //  //notify poster's followers of his new comment
+    //  _.each(followers, function (id) {
+    //    if(id !== options.replyTo) { //if it's not the replyTo user (whom we should've already notified)
+    //      Meteor.users.update(id, {$addToSet: {"notifications.followingUser": notobj}});
+    //    }
+    //  });
+
+    //  var topicFollowers = [];
+    //  Meteor.users.find({"activity.followingTopics": options.topicId}).forEach(function (user) {
+    //    if(user._id !== options.replyTo && user._id !== userId) {
+    //      topicFollowers.push(user._id);
+    //    }
+    //  });
+
+    //  notobj.url = "/topics/" + options.topicId;
+    //  notobj.message = "A new comment was posted in '" + topicTitle + "'";
+      
+    //  // notify people who are following the topic in which there is the new comment, 
+    //  // but not if they have already been notified via the user whom they are following
+    //  _.each(_.difference(topicFollowers, followers), function (id) {
+    //    // "a new comment has been posted in topic"
+    //    var currUrl = "/topics/" + options.topicId; //url for this notification
+
+    //    // check if a notification entry already has this url
+    //    notif = _.find(Meteor.users.findOne(id).notifications.followingTopic, function (obj) { 
+    //      return obj.url === currUrl; 
+    //    }); 
+
+    //    //if this topic already has more than one notification entry with the url, combine them all into one with a "count" 
+    //    if(notif) { //if this topic already has one entry
+    //      if(notif.count) {
+    //        Meteor.users.update({"_id": id, "notifications.followingTopic": {$elemMatch: {"url": currUrl}}}, 
+    //                            {$inc: {"notifications.followingTopic.$.count": 1}});
+    //      } else {
+    //        notobj.count = 2;
+    //        notobj.message = " new comments were posted in '" + topicTitle + "'";
+    //        Meteor.users.update(id, {$pull: {"notifications.followingTopic": {"url": currUrl}}});
+    //        Meteor.users.update(id, {$push: {"notifications.followingTopic": notobj}}); 
+    //      }
+    //    } 
+    //    //"count" is still low, so just add it as usual, as a normal notification entry
+    //    else
+    //      Meteor.users.update(id, {$addToSet: {"notifications.followingTopic": notobj}});
+    //  });
+    // } else if (type === "newTopic") {
+    //  _.each(Meteor.users.findOne(userId).activity.followers, function (id) {
+    //    Meteor.users.update(id, {$addToSet: {"notifications.followingUser": 
+    //      {"url": "/topics/" + options.topicId, "message": name + " created the new topic '" + options.topicTitle + "'"}}});
+    //  });
+    // }
+  },
+});
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
