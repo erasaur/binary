48fdfe4f1e31a59a548b5188ac5f8c59ea020c2f
markblee <mmaarrkklleeee@gmail.com>
2014-10-07 23:56:24 -0700

    Style h3 globally, allow clicking on follow button in profileItem, disallow following self

--- client/views/profile/profile_item.js
+++ client/views/profile/profile_item.js
@@ -1,6 +1,18 @@
 Template.profileItem.helpers({
+  canFollow: function () {
+    return Meteor.userId() && this._id != Meteor.userId();
+  },
   following: function () {
     if (Meteor.user() && Meteor.user().activity)
     return Meteor.user().activity.followingUsers.indexOf(this._id) > -1;
   }
-})
+});
+
+Template.profileItem.events({
+  'click #js-follow': function (event, template) {
+    Meteor.call("newFollower", this._id);
+  },
+  'click #js-unfollow': function (event, template) {
+    Meteor.call("removeFollower", this._id);
+  }
+});
