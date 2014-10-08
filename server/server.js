0487a2ff28857574fa7930f28e7a9feb9797fe02
markblee <mmaarrkklleeee@gmail.com>
2014-10-07 23:39:09 -0700

    Add followersCount to account stats, fix upvote/downvote methods

--- server/server.js
+++ server/server.js
@@ -81,24 +81,30 @@ Meteor.methods({
 	},
 	newFollower: function (following) {
     var userId = this.userId;
-		Meteor.users.update(userId, { $addToSet: { "activity.followingUsers": following } });
-		Meteor.users.update(following, { $addToSet: { "activity.followers": userId } });
+		Meteor.users.update(userId, { $addToSet: { 'activity.followingUsers': following } });
+		Meteor.users.update(following, { 
+      $addToSet: { 'activity.followers': userId },
+      $inc: { 'stats.followersCount': 1 } 
+    });
 	},
 	removeFollower: function (following) {
     var userId = this.userId;
-		Meteor.users.update(userId, { $pull: { "activity.followingUsers": following } });
-		Meteor.users.update(following, { $pull: { "activity.followers": userId } });
+		Meteor.users.update(userId, { $pull: { 'activity.followingUsers': following } });
+		Meteor.users.update(following, { 
+      $pull: { 'activity.followers': userId },
+      $inc: { 'stats.followersCount': -1 } 
+    });
 	},
 	upvoteComment: function (commentId, ownerId) {
     var userId = this.userId;
 		Comments.update(commentId, { $inc: { "upvotes": 1 } });
-		Meteor.users.update(ownerId, { $inc: { "reputation": 1 } });
+		Meteor.users.update(ownerId, { $inc: { "stats.reputation": 1 } });
 		Meteor.users.update(userId, { $addToSet: { "activity.upvotedComments": commentId } });
 	},
 	downvoteComment: function (commentId, ownerId) {
     var userId = this.userId;
 		Comments.update(commentId, { $inc: { "upvotes": -1 } });
-		Meteor.users.update(ownerId, { $inc: {"reputation": -1 } });	
+		Meteor.users.update(ownerId, { $inc: { "stats.reputation": -1 } });	
 		Meteor.users.update(userId, { $pull: { "activity.upvotedComments": commentId } });
 	},
 	followTopic: function (topicId) {
