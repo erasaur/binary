9531e330d9539489697f2ad9955d59d347c86d8d
markblee <mmaarrkklleeee@gmail.com>
2014-10-08 17:52:30 -0700

    Add sort by param to topic route and publication

--- server/publications/single_topic.js
+++ server/publications/single_topic.js
@@ -1,36 +1,38 @@
 // Publish a single topic
 
-// Meteor.publish("singleTopic", function (topicId) {
+// Meteor.publish('singleTopic', function (topicId) {
 //   // all comments
-//   var comments = Comments.find({"topicId": topicId}, {sort: {"likes": -1}});
-//   var userIds = _.pluck(comments.fetch(), "userId"); // comment owners
+//   var comments = Comments.find({'topicId': topicId}, {sort: {'likes': -1}});
+//   var userIds = _.pluck(comments.fetch(), 'userId'); // comment owners
 
 //   // add topic owner to list of userIds
 //   var topicOwner = Topics.findOne(topicId).userId;
 //   userIds.push(topicOwner);
 
 //   // owners of all comments
-//   var users = Meteor.users.find({"_id": {$in: userIds}}, {fields: {"username": 1}});
+//   var users = Meteor.users.find({'_id': {$in: userIds}}, {fields: {'username': 1}});
 
 //   return [comments, users];
 // });
 
-Meteor.publish("singleTopic", function (topicId) {
+Meteor.publish('singleTopic', function (topicId, sortBy) {
+  var sortBy = sortBy || 'upvotes';
+
   Meteor.publishWithRelations({
     handle: this,
     collection: Topics,
     filter: topicId,
     mappings: [{
-      key: "userId", collection: Meteor.users, // publish topic owner
-      options: { fields: { "username": 1, "profile": 1 }, limit: 1 }
+      key: 'userId', collection: Meteor.users, // publish topic owner
+      options: { fields: { 'username': 1, 'profile': 1 }, limit: 1 }
     }, {
       reverse: true, // use the comments topicId to match this _id
-      key: "topicId",
+      key: 'topicId',
       collection: Comments,
-      options: { sort: { "upvotes": -1 } },
+      options: { sort: { sortBy: -1 } },
       mappings: [{
-        key: "userId", collection: Meteor.users, // publish comment owners
-        options: { fields: { "username": 1, "profile": 1, "stats": 1 } }
+        key: 'userId', collection: Meteor.users, // publish comment owners
+        options: { fields: { 'username': 1, 'profile': 1, 'stats': 1 } }
       }]
     }]
   });
