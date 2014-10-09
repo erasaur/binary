9b19c450b33e318af25af57c6977e0a900730459
markblee <mmaarrkklleeee@gmail.com>
2014-10-08 17:52:30 -0700

    Add sort by param to topic route and publication

--- client/routes/routes.js
+++ client/routes/routes.js
@@ -1,26 +1,26 @@
 Router.configure({
-	layoutTemplate: "mainLayout",
+	layoutTemplate: 'mainLayout',
 	yieldTemplates: {
-		"nav": {to: "nav"}
+		'nav': {to: 'nav'}
 	},
-  loadingTemplate: "loading",
-  notFoundTemplate: "notFound"
+  loadingTemplate: 'loading',
+  notFoundTemplate: 'notFound'
 });
 
 Router.onBeforeAction(function () {
   if (!Meteor.loggingIn() && !Meteor.user())
-    this.redirect("signup");
+    this.redirect('signup');
 
-}, {except: ["signup", "login"]}); //forgot password page
+}, {except: ['signup', 'login']}); //forgot password page
 
 Router.onBeforeAction(function () {
   if (Meteor.user())
-    this.redirect("home");
+    this.redirect('home');
 
-}, {only: ["signup", "login"]});
+}, {only: ['signup', 'login']});
 
-Router.onBeforeAction("loading");
-Router.onBeforeAction("dataNotFound");
+Router.onBeforeAction('loading');
+Router.onBeforeAction('dataNotFound');
 
 Router.onAfterAction(function () {
   if (this.ready()) {
@@ -35,65 +35,65 @@ Router.onAfterAction(function () {
 });
 
 Deps.autorun(function () {
-  Meteor.subscribe("topicsList", Session.get("topicsLimit"));  
+  Meteor.subscribe('topicsList', Session.get('topicsLimit'));  
 });
 
-Meteor.subscribe("currentUser");
+Meteor.subscribe('currentUser');
 
 Router.map(function() {
-	this.route("home", { 
-    path: "/",
+	this.route('home', { 
+    path: '/',
     waitOn: function () {
-      return Meteor.subscribe("topicsList", Session.get("topicsLimit"));
+      return Meteor.subscribe('topicsList', Session.get('topicsLimit'));
     }		
   });
-  this.route("signup", {yieldTemplates: {}}); //don't yield nav
-  this.route("login", {yieldTemplates: {}}); //don't yield nav
-  this.route("profile", {
-    path: "/users/:_id",
-    layoutTemplate: "pageLayout",
+  this.route('signup', {yieldTemplates: {}}); //don't yield nav
+  this.route('login', {yieldTemplates: {}}); //don't yield nav
+  this.route('profile', {
+    path: '/users/:_id',
+    layoutTemplate: 'pageLayout',
     yieldTemplates: { 
-      "nav": { to: "nav" },
-      "profileButtons": { to: "pageButtons" },
-      "profileHeader": { to: "pageHeader" },
-      "profileNav": { to: "pageNav" } 
+      'nav': { to: 'nav' },
+      'profileButtons': { to: 'pageButtons' },
+      'profileHeader': { to: 'pageHeader' },
+      'profileNav': { to: 'pageNav' } 
     },
     waitOn: function () {
-      return Meteor.subscribe("userProfile", this.params._id);
+      return Meteor.subscribe('userProfile', this.params._id);
     },
     onRun: function () {
-      Session.set("currentTab", "profileComments");
+      Session.set('currentTab', 'profileComments');
     },
     data: function () {
       return Meteor.users.findOne(this.params._id);
     }  
   });
-  this.route("topic", { 
-  	path: "/topics/:_id",
-    layoutTemplate: "pageLayout",
+  this.route('topic', { 
+  	path: '/topics/:_id',
+    layoutTemplate: 'pageLayout',
     yieldTemplates: { 
-      "nav": { to: "nav" },
-      "topicButtons": { to: "pageButtons" },
-      "topicHeader": { to: "pageHeader" },
-      "topicNav": { to: "pageNav" } 
+      'nav': { to: 'nav' },
+      'topicButtons': { to: 'pageButtons' },
+      'topicHeader': { to: 'pageHeader' },
+      'topicNav': { to: 'pageNav' } 
     },
   	waitOn: function () {
-  		return Meteor.subscribe("singleTopic", this.params._id);
+  		return Meteor.subscribe('singleTopic', this.params._id, this.params.sort_by);
   	},
     onRun: function () {
-      Session.set("currentTab", "topicComments");
-      SessionAmplify.set("showingReplies", []);
+      Session.set('currentTab', 'topicComments');
+      SessionAmplify.set('showingReplies', []);
     },
   	data: function () {
-      Session.set("currentTopic", this.params._id);
+      Session.set('currentTopic', this.params._id);
 			return Topics.findOne(this.params._id);
   	}
   });
-  this.route("settings", {
-    path: "/users/:_id/settings"
+  this.route('settings', {
+    path: '/users/:_id/settings'
   });
-  this.route("notFound", {
-    path: "*"
+  this.route('notFound', {
+    path: '*'
   });
 });
 
