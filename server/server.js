304cbe57a233b70cb476ac0f71e7f21b8a677d21
markblee <mmaarrkklleeee@gmail.com>
2014-10-09 23:09:38 -0700

    Move following methods from server, add permissions checking

--- server/server.js
+++ server/server.js
@@ -1,20 +1,4 @@
 Meteor.methods({
-	newFollower: function (following) {
-    var userId = this.userId;
-		Meteor.users.update(userId, { $addToSet: { 'activity.followingUsers': following } });
-		Meteor.users.update(following, { 
-      $addToSet: { 'activity.followers': userId },
-      $inc: { 'stats.followersCount': 1 } 
-    });
-	},
-	removeFollower: function (following) {
-    var userId = this.userId;
-		Meteor.users.update(userId, { $pull: { 'activity.followingUsers': following } });
-		Meteor.users.update(following, { 
-      $pull: { 'activity.followers': userId },
-      $inc: { 'stats.followersCount': -1 } 
-    });
-	},
 	followTopic: function (topicId) {
     var userId = this.userId;
 		Topics.update(topicId, { $addToSet: { "followers": userId } });
