f57e9b856bfeb0252b2c58a135e33bbd062a5388
markblee <mmaarrkklleeee@gmail.com>
2014-10-14 10:00:14 -0700

    Change instances of UI to Template

--- client/helpers/helpers.js
+++ client/helpers/helpers.js
@@ -1,15 +1,15 @@
-UI.registerHelper("currentPage", function (path) {
+Template.registerHelper("currentPage", function (path) {
 	return getCurrentRoute() === path;
 });
-UI.registerHelper("formatDate", function (date) {
+Template.registerHelper("formatDate", function (date) {
 	return formatDate(date);
 });
-UI.registerHelper("formatName", function (userId) {
+Template.registerHelper("formatName", function (userId) {
   return getDisplayNameById(userId);
 });
 
 //pass in an ID from handlebars, and get a collection back
-// UI.registerHelper("toCollection", function(context, options) {
+// Template.registerHelper("toCollection", function(context, options) {
 // 	if(context && options)
 // 		return idToCollection(context, options);
 // });
