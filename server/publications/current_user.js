4f91a4678637e4cdc6444b2c25e6a32e5b8b4200
markblee <mmaarrkklleeee@gmail.com>
2014-10-19 22:07:16 -0700

    Publish currentUser invites info

--- server/publications/current_user.js
+++ server/publications/current_user.js
@@ -2,6 +2,6 @@
 
 Meteor.publish('currentUser', function () {
   return Meteor.users.find(this.userId, { 
-    fields: { 'email_hash': 1, 'stats': 1, 'activity': 1 } 
+    fields: { 'invites': 1, 'email_hash': 1, 'stats': 1, 'activity': 1 } 
   });
 });
