d052718dfe3c18923d43cc09b080d803f0b01fc7
markblee <mmaarrkklleeee@gmail.com>
2014-10-22 10:48:33 -0700

    Call done callback to enable auto logins after clicking on email verification link

--- client/main.js
+++ client/main.js
@@ -13,7 +13,8 @@ Accounts.onEmailVerificationLink(function (token, done) {
   Accounts.verifyEmail(token, function (error) {
     if (error) 
       alert(formatError(error));
-    else
+    else 
       alert('Success! Your email has been verified.');
   });
+  done();
 });
