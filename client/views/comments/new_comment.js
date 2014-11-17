988c7dcb96612aca07819d1fc7b9cce6047ab556
markblee <mmaarrkklleeee@gmail.com>
2014-11-17 15:58:45 -0800

    Remove medium style editing

--- client/views/comments/new_comment.js
+++ client/views/comments/new_comment.js
@@ -1,16 +1,16 @@
-var editors = {};
+// var editors = {};
 
-Template.newComment.rendered = function () {
-  editors[this.data.id] = new MediumEditor('.editable', {
-    buttonLabels: 'fontawesome',
-    buttons: ['bold', 'italic', 'anchor', 'pre', 'header1', 'quote', 'unorderedlist'],
-    checkLinkFormat: true,
-    cleanPastedHTML: true,
-    disableDoubleReturn: true,
-    firstHeader: 'h3'
-  });
-};
+// Template.newComment.rendered = function () {
+//   editors[this.data.id] = new MediumEditor('.editable', {
+//     buttonLabels: 'fontawesome',
+//     buttons: ['bold', 'italic', 'anchor', 'pre', 'header1', 'quote', 'unorderedlist'],
+//     checkLinkFormat: true,
+//     cleanPastedHTML: true,
+//     disableDoubleReturn: true,
+//     firstHeader: 'h3'
+//   });
+// };
 
-Template.newComment.destroyed = function () {
-  editors[this.data.id].deactivate();
-};
+// Template.newComment.destroyed = function () {
+//   editors[this.data.id].deactivate();
+// };
