4c3dc79f78214b41196789ad907998b247a7f0b0
markblee <mmaarrkklleeee@gmail.com>
2014-11-17 23:25:37 -0800

    Update routes and prevent flickering when trying to view topics/users when logged out

--- client/routes/routes.js
+++ client/routes/routes.js
@@ -1,15 +1,26 @@
+var subs = new SubsManager({
+  // cache recent 50 subscriptions
+  cacheLimit: 50,
+  // expire any subscription after 30 minutes
+  expireIn: 30
+});
+
 Router.configure({
-	layoutTemplate: 'mainLayout',
+  layoutTemplate: 'mainLayout',
   loadingTemplate: 'loading',
-  notFoundTemplate: 'notFound'
+  notFoundTemplate: 'notFound',
+  waitOn: function () {
+    return subs.subscribe('currentUser');
+  }
 });
 
 Router.onBeforeAction(function () {
   if (!Meteor.loggingIn() && !Meteor.user()) {
-    if (Session.get('resetPassword'))
-      this.redirect('forgotPassword');
-    else {
-      this.redirect('landing');
+    if (Session.get('resetPassword')) {
+      this.render('forgotPassword');
+    } else {
+      this.render('notFound');
+      this.stop();
     }
   } else {
     this.next();
@@ -29,8 +40,8 @@ Router.onBeforeAction(function () {
 
 // }, { only: ['admin'] });
 
-Router.onBeforeAction('loading');
-Router.onBeforeAction('dataNotFound');
+Router.plugin('loading', { notFoundTemplate: 'loading' });
+Router.plugin('dataNotFound', { notFoundTemplate: 'notFound' });
 
 Router.onAfterAction(function () {
   if (this.ready()) {
@@ -44,77 +55,88 @@ Router.onAfterAction(function () {
   }
 });
 
-var subs = new SubsManager({
-  // cache recent 50 subscriptions
-  cacheLimit: 50,
-  // expire any subscription after 30 minutes
-  expireIn: 30
+Router.route('/forgot', { 
+  name: 'forgotPassword'
 });
+Router.route('/invite', {
+  onBeforeAction: function () {
+    var self = this;
+    var inviterId = self.params.inviter_id;
+    var inviteCode = self.params.invite_code;
 
-Tracker.autorun(function () {
-  subs.subscribe('topicsList', Session.get('topicsLimit'));  
+    Meteor.call('validLink', inviterId, inviteCode, function (error, result) {
+      if (!result || error)
+        self.render('notFound');
+    });
+  }
 });
+Router.route('/login');
+Router.route('/landing');
 
-subs.subscribe('currentUser');
-
-Router.map(function() {
-	this.route('home', { 
-    path: '/',
-    waitOn: function () {
-      return subs.subscribe('topicsList', Session.get('topicsLimit'));
-    }		
-  });
-  this.route('forgotPassword', { // don't yield nav
-    path: '/forgot'
-  });
-  this.route('invite', { // don't yield nav
-    onBeforeAction: function () {
-      var self = this;
-      var inviterId = self.params.inviter_id;
-      var inviteCode = self.params.invite_code;
-      Meteor.call('validLink', inviterId, inviteCode, function (error, result) {
-        if (!result || error)
-          self.render('notFound');
-      });
+Router.route('/', { 
+  name: 'home',
+  waitOn: function () {
+    return subs.subscribe('topicsList', Session.get('topicsLimit'));
+  },
+  action: function () {
+    if (Meteor.user()) {
+      this.render('nav', { to: 'nav' });
+      this.render('home');
     }
-  }); 
-  this.route('login'); // don't yield nav
-  this.route('landing'); // don't yield nav
-  this.route('profile', {
-    path: '/users/:_id',
-    layoutTemplate: 'pageLayout',
-    waitOn: function () {
-      return Meteor.subscribe('userProfile', this.params._id);
-    },
-    onRun: function () {
-      Session.set('currentTab', 'profileComments');
-    },
-    data: function () {
-      return Meteor.users.findOne(this.params._id);
-    }  
-  });
-  this.route('topic', { 
-  	path: '/topics/:_id',
-    layoutTemplate: 'pageLayout',
-  	waitOn: function () {
-  		return [
-        Meteor.subscribe('singleTopic', this.params._id),
-        Meteor.subscribe('topicComments', this.params._id, this.params.sort_by),
-      ];
-  	},
-    onRun: function () {
+  },
+  data: function () {
+    return Topics.find({}, { sort: { 'createdAt': -1 } });
+  }
+});
+Router.route('/topics/:_id', {
+  name: 'topic',
+  waitOn: function () {
+    return [
+      Meteor.subscribe('singleTopic', this.params._id),
+      Meteor.subscribe('topicComments', this.params._id, this.params.sort_by),
+    ];
+  },
+  action: function () {
+    if (Meteor.user()) {
       Session.set('currentTab', 'topicComments');
       SessionAmplify.set('showingReplies', []);
-    },
-  	data: function () {
-      Session.set('currentTopic', this.params._id);
-			return Topics.findOne({ '_id': this.params._id, 'isDeleted': false });
-  	}
-  });
-  this.route('settings', {
-    // the _id is for show and not to be used in any queries
-    path: '/users/:_id/settings'
-  });
+      this.layout('pageLayout');
+      this.render();
+      this.render('nav', { to: 'nav' });
+      this.render('topicButtons', { to: 'pageButtons' });
+      this.render('topicHeader', { to: 'pageHeader' });
+      this.render('topicNav', { to: 'pageNav' });
+    }
+  },
+  data: function () {
+    Session.set('currentTopic', this.params._id);
+    return Topics.findOne({ '_id': this.params._id, 'isDeleted': false });
+  }
+});
+Router.route('/users/:_id', {
+  name: 'profile',
+  waitOn: function () {
+    return Meteor.subscribe('userProfile', this.params._id);
+  },
+  action: function () {
+    if (Meteor.user()) {
+      Session.set('currentTab', 'profileComments');
+      this.layout('pageLayout');
+      this.render();
+      this.render('nav', { to: 'nav' });
+      this.render('profileButtons', { to: 'pageButtons' });
+      this.render('profileHeader', { to: 'pageHeader' });
+      this.render('profileNav', { to: 'pageNav' });
+    } else {
+      console.warn('gtfo');
+    }
+  },
+  data: function () {
+    return Meteor.users.findOne(this.params._id);
+  }  
+});
+Router.route('/users/:_id/settings', {
+  name: 'settings'
 });
 
 
