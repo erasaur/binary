ce41d6d15627a59172e28ceb9ecf1137c77bf5e0
markblee <mmaarrkklleeee@gmail.com>
2014-11-18 16:00:54 -0800

    Improve publications and remove old code

--- server/publications/topics_list.js
+++ server/publications/topics_list.js
@@ -1,52 +1,10 @@
 // Publish list of topics (sorted by date) and top comment for each
 
-// Meteor.publish('topicsList', function (limit) {  
-//   if (!canViewById(this.userId)) return [];
-
-//   // set the topic display limit
-//   if (limit > Topics.find().count() || !limit)
-//     limit = 0;
-
-//   // get the topics cursor and store the ids
-//   var topics = Topics.find({ 'isDeleted': false }, { limit: limit, sort: { 'createdAt': -1 } });
-//   var topicsArray = topics.fetch();
-//   var topicIds = _.pluck(topicsArray, '_id');
-
-//   // get the owners of each topic
-//   var userIds = _.pluck(topicsArray, 'userId');
-//   var users = Meteor.users.find({ '_id': { $in: userIds } }, { 
-//     fields: { 'email_hash': 1, 'profile': 1 } 
-//   });
-
-//   // get the top comment id of each topic
-//   var commentIds = [];
-
-//   for (var i = topicIds.length - 1; i >= 0; i--) {
-//     var comment = Comments.findOne({ 'topicId': topicIds[i], 'isDeleted': false }, {
-//       sort: { 'upvotes': -1 }
-//     });
-
-//     if (comment)
-//       commentIds.push(comment._id);
-//   }
-
-//   // find the top comments
-//   var topComments = Comments.find({ '_id': { $in: commentIds } });
-
-//   return [topics, topComments, users];
-// });
-
 Meteor.publishComposite('topicsList', function (limit) {
-  if (!canViewById(this.userId)) {
-    return {
-      find: function () {
-        return;
-      }
-    };
-  } 
-
   return {
     find: function () {
+      if (!canViewById(this.userId)) return;
+
       return Topics.find({ 'isDeleted': false }, { sort: { 'createdAt': -1 }, limit: limit });
     },
     children: [
--- server/publications/user_profile.js
+++ server/publications/user_profile.js
@@ -1,106 +1,10 @@
 // Publish profile page
 
-// Meteor.publish('userProfile', function (userId) {
-//   if (!canViewById(this.userId)) return [];
-
-//   // we will have to look up user twice: once to get the 
-//   // followers/following users, and once again in the final
-//   // return statement. this is because we can't publish
-//   // user separately from user's followers, since we can
-//   // only return one cursor from each collection
-//   var user = Meteor.users.findOne(userId);
-
-//   /** 
-//    * Publish all comments created or liked by user
-//    */
-//   var commentIds = [];
-
-//   if (user && user.activity)  
-//     commentIds = user.activity.upvotedComments;
-
-//   var comments = Comments.find({ 
-//     $or: [ { 'userId': userId }, { '_id': { $in: commentIds } } ], 
-//     isDeleted: false
-//   });
-
-//   /** 
-//    * Publish all topics created or followed by user, or 
-//    * topics related to the comments above
-//    */
-//   var topicIds = [];
-//   if (typeof comments !== 'undefined')
-//     topicIds = _.pluck(comments.fetch(), 'topicId');
-
-//   if (user.activity && user.activity.followingTopics)
-//     topicIds = _.union(topicIds, user.activity.followingTopics);
-
-//   var topics = Topics.find({ 
-//     $or: [ { 'userId': userId }, { '_id': { $in: topicIds } } ]
-//   }, { 
-//     fields: { '_id': 1, 'title': 1, 'createdAt': 1, 'userId': 1, 'pro': 1, 'con': 1 } 
-//   });
-
-//   /** 
-//    * Publish all comments (including top comments for topics)
-//    */
-//   var topCommentIds = [];
-
-//   for (var i = topicIds.length - 1; i >= 0; i--) {
-//     var comment = Comments.findOne({ 'topicId': topicIds[i] }, {
-//       sort: { 'upvotes': -1 }
-//     });
-
-//     if (comment)
-//       topCommentIds.push(comment._id);
-//   }
-
-//   if (topCommentIds.length)
-//     commentIds = _.union(commentIds, topCommentIds);
-
-//   comments = Comments.find({ 
-//     $or: [ { 'userId': userId }, { '_id': { $in: commentIds } } ], 
-//     isDeleted: false
-//   });
-
-//   /** 
-//    * Publish all followers and following users, and 
-//    * all users related to the comments or topics above
-//    */
-//   var userIds = [];
-
-//   if (user && user.activity)
-//     userIds = _.union(user.activity.followers, user.activity.followingUsers);
-
-//   userIds.push(userId); // add back the original user
-
-//   var commentUsers = [];
-//   var topicUsers = [];
-//   if (typeof comments !== 'undefined')
-//     commentUsers = _.pluck(comments.fetch(), 'userId');
-
-//   if (typeof topics !== 'undefined')
-//     topicUsers = _.pluck(topics.fetch(), 'userId');
-
-//   userIds = _.union(userIds, commentUsers, topicUsers);
-
-//   var users = Meteor.users.find({ '_id': { $in: userIds } }, {
-//     fields: { 'email_hash': 1, 'profile': 1, 'stats': 1, 'activity': 1 }
-//   });
-
-//   return [comments, topics, users];
-// });
-
 Meteor.publishComposite('userProfile', function (userId) {
-  if (!canViewById(this.userId)) {
-    return {
-      find: function () {
-        return;
-      }
-    };
-  }
-
   return {
     find: function () { // the user
+      if (!canViewById(this.userId)) return;
+
       return Meteor.users.find(userId, { 'limit': 1, 'fields': { 
         'email_hash': 1, 'profile': 1, 'stats': 1, 'activity': 1 
       }});
