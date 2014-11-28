d0a6cb004e3fb771aee86785edfc48ef6c8c73c9
Mark Lee <marklee.lhs@gmail.com>
2014-11-27 23:17:39 -0800

    Style inputs and collapsed navbar toggle

--- client/views/settings/settings.js
+++ client/views/settings/settings.js
@@ -1,15 +1,3 @@
-// // button jquery object
-// function displaySaved ($button) {
-//   var button = $button.find('span');
-//   var text = button.text(); // original text to be restored
-
-//   button.fadeOut(300, function () {
-//     button.text('Saved!').fadeIn(300).delay(2000).fadeOut(300, function () {
-//       button.text(text).fadeIn(300);
-//     });
-//   });
-// }
-
 Template.settingsBody.created = function () {
   this.editing = new ReactiveVar(false);
 };
@@ -61,52 +49,7 @@ Template.settings.events({
       // eg { 'profile.notifications.couriers.newComment.follower.onsite': true }
       Meteor.call('changePreferences', setProperty({}, actionValue, !!newValue));
     }
-  },
-  
-  // 'click #js-edit-name, click #js-edit-bio, click #js-edit-email': function (event, template) {
-  //   var target = event.currentTarget;
-  //   var id = target.id;
-  //   var fieldName = id.substring(id.lastIndexOf('-') + 1); // js-edit-xxx -> xxx
-  //   var fieldId = '#js-' + fieldName; // js-xxx
-  //   var newValue = template.find(fieldId).value;
-
-  //   var method = 'change' + fieldName.charAt(0).toUpperCase() + fieldName.substring(1);
-
-  //   Meteor.call(method, newValue, function (error) {
-  //     if (error) {
-  //       console.log(error.invalidKeys);
-  //       alert('Sorry, please try to stick to alphanumeric characters, hyphens, periods, and apostrophes!');
-  //     }
-  //     else {
-  //       displaySaved($(target));
-  //       if (fieldName === 'email')
-  //         alert('Please check your email for a verification link. Thanks!');
-  //     }
-  //   });
-  // },
-  // 'click #js-save-password': function (event, template) {
-  //   var oldPassword = template.find('#js-password').value;
-  //   var newPassword = template.find('#js-newPassword').value;
-
-  //   if (newPassword.length < 6)
-  //     alert('Your password must be at least 6 characters long.');
-  //   else {
-  //     Accounts.changePassword(oldPassword, newPassword, function (error) {
-  //       if (error)
-  //         alert('Please verify that you have entered the correct password.');
-  //       else {
-  //         $('#js-newPassword').slideUp('fast', function () {
-  //           $('#js-password').prop('disabled', true).val('○○○○○○○');
-  //           $('#js-newPassword').val('');
-  //           $('#js-editing-password').hide();
-
-  //           displaySaved($('#js-edit-password'));
-  //           $('#js-edit-password').show();
-  //         });
-  //       }
-  //     });  
-  //   }
-  // }
+  }
 });
 
 
