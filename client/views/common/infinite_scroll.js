e80d9abe23adc3fd47bba7ca337be917d16e0393
Mark Lee <marklee.lhs@gmail.com>
2014-12-02 00:33:32 -0800

    Accept multiple cursors for infinite scrolling

--- client/views/common/infinite_scroll.js
+++ client/views/common/infinite_scroll.js
@@ -1,29 +1,52 @@
-initInfiniteScroll = function (cursor) {
-  var count = 0;
+function InfiniteScroll (cursor) {
+  var self = this;
 
   // observeChanges will fire for initial set, so count can start at 0
-  this._infiniteScroll = cursor.observeChanges({
+  self.count = 0;
+  var cursor = cursor.observeChanges({
     added: function () {
-      count++;
+      self.count++;
     },
     removed: function () {
-      count--;
+      self.count--;
     }
   });
 
-  console.log('items limit reset');
-  Session.set('itemsLimit', 2);
+  this.stop = function () {
+    console.log(cursor);
+    cursor.stop();
+  };
+}
+
+initInfiniteScroll = function (cursors) {
+  var cursors = Object.prototype.toString.call(cursors) === '[object Array]' ? 
+    cursors : [cursors];
+  var self = this;
+
+  self._infiniteScroll = self._infiniteScroll || [];
+
+  _.each(cursors, function (cursor) {
+    var obj = new InfiniteScroll(cursor);
+    console.log(obj);
+    self._infiniteScroll.push(obj);
+  });
+
+  if (!Session.get('itemsLimit')) {
+    console.log('items limit reset');
+    Session.set('itemsLimit', 2);
+  }
 
   $(window).on('scroll', _.throttle(function () {
     // trigger at 300px above bottom
     var target = document.body.offsetHeight - 300;
 
     if (window.innerHeight + window.scrollY >= target) {
-      console.log(count, Session.get('itemsLimit'))
-
-      if (count >= Session.get('itemsLimit')) {
-        Session.set('itemsLimit', Session.get('itemsLimit') + 2); //fetch more items from server
-      }
+      _.each(self._infiniteScroll, function (obj) {
+        console.log(obj.count, Session.get('itemsLimit'));
+        if (obj.count >= Session.get('itemsLimit')) {
+          Session.set('itemsLimit', Session.get('itemsLimit') + 1); //fetch more items from server
+        }
+      });
     }
   }, 300));  
 };
@@ -31,5 +54,7 @@ initInfiniteScroll = function (cursor) {
 stopInfiniteScroll = function () {
   console.log('search and destroy!');
   $(window).off('scroll');
-  this._infiniteScroll && this._infiniteScroll.stop();
+  _.each(this._infiniteScroll, function (obj) {
+    obj.stop();
+  });
 };
