86d3a55f0175464aa5eb9db6d2bfacb2bb2c4fbc
Mark Lee <marklee.lhs@gmail.com>
2014-12-07 15:12:34 -0800

    Update to nested couriers and use herald functions for notification preferences

--- client/views/settings/settings.js
+++ client/views/settings/settings.js
@@ -21,7 +21,7 @@ Template.settingsBody.events({
 Template.settings.helpers({
   'isEnabled': function (option, value) {
     var userId = Meteor.userId();
-    
+
     return userId && Herald.userPreference(userId, 'email', option) == value ? 
       'btn-primary' : 'btn-default';
   }
@@ -31,22 +31,24 @@ Template.settings.events({
   'click button[data-action="toggle-setting"]': function (event, template) {
     var button = event.currentTarget;
     var actionValue = button.getAttribute('data-action-value');
-    var newValue = parseInt(button.getAttribute('value')); // 0 or 1
-    var medium = '.onsite'; // medium of delivering notifications
+    var newValue = !!parseInt(button.getAttribute('value')); // 0 or 1
+    // var medium = '.onsite'; // medium of delivering notifications
 
     // toggling global notifications
     if (!actionValue) {
       // use Herald setUserPreference ?
-      Meteor.call('changePreferences', setProperty({}, 'profile.notifications.media' + medium, !!newValue));
+      // Meteor.call('changePreferences', setProperty({}, 'profile.notifications.media' + medium, !!newValue));
+      Herald.setUserPreference(Meteor.user(), { 'email': newValue });
     } 
     // toggling courier specific preferences
     else {
       // construct the query key
       actionValue = actionValue.replace(/-/g, '.');
-      actionValue = 'profile.notifications.couriers.' + actionValue + medium;
+      // actionValue = 'profile.notifications.couriers.' + actionValue + medium;
 
       // eg { 'profile.notifications.couriers.newComment.follower.onsite': true }
-      Meteor.call('changePreferences', setProperty({}, actionValue, !!newValue));
+      // Meteor.call('changePreferences', setProperty({}, actionValue, !!newValue));
+      Herald.setUserPreference(Meteor.user(), { 'email': newValue }, actionValue);
     }
   }
 });
