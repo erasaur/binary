a7536fb1d81017c643e16f675df71d867b156d4c
Mark Lee <marklee.lhs@gmail.com>
2014-12-09 14:22:59 -0800

    Update pubs for flags

--- server/publications/current_user.js
+++ server/publications/current_user.js
@@ -3,7 +3,12 @@
 Meteor.publish('currentUser', function () {
   if (!this.userId) return this.ready(); 
 
-  return Meteor.users.find(this.userId, { 
-    fields: { 'invites': 1, 'email_hash': 1, 'stats': 1, 'activity': 1, 'isAdmin': 1 } 
-  });
+  var fields = { 
+    'invites': 1, 
+    'email_hash': 1, 
+    'stats': 1, 
+    'activity': 1, 
+    'isAdmin': 1
+  }; 
+  return Meteor.users.find(this.userId, { fields: fields });
 });
