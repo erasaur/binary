e272eeb5d5f5d1482d316b05ad98eb6c46a71fad
Mark Lee <marklee.lhs@gmail.com>
2014-12-09 14:22:36 -0800

    Add admin page for viewing flags

---
+++ client/views/admin/admin.js
@@ -0,0 +1,36 @@
+Template.admin.helpers({
+  flags: function () {
+    return Flags.find();
+  },
+  user: function () {
+    return Meteor.users.findOne(this.userId);
+  },
+  item: function () {
+    var collection = {
+      'comments': Comments,
+      'topics': Topics
+    };
+    var item = collection[this.itemType].findOne(this.itemId);
+    item.isCommentItem = true;
+    return item;
+  },
+  itemTemplate: function () {
+    var template = {
+      'comments': 'comment',
+      'topics': 'topicItem'
+    };
+    return template[this.itemType];
+  },
+  statusClass: function () {
+    return this.status === 1 ? 'js-undo-helpful helpful' : 'js-helpful';
+  }
+});
+
+Template.admin.events({
+  'click .js-helpful': function (event, template) {
+    Meteor.call('changeFlag', this, 1);
+  },
+  'click .js-undo-helpful': function (event, template) {
+    Meteor.call('changeFlag', this, 0);
+  }
+});
