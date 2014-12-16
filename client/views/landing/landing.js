d7b8bde4b9ba01329874ac63aa94fa0e9476557f
Mark Lee <marklee.lhs@gmail.com>
2014-12-16 15:37:13 -0800

    Fade in animation

--- client/views/landing/landing.js
+++ client/views/landing/landing.js
@@ -6,14 +6,19 @@ Template.landing.rendered = function () {
   var $pro = this.$('#landing-image-pro');
   var $con = this.$('#landing-image-con');
 
-  function animate () {
+  // fade content in
+  this.$('.landing-container').css('opacity', 0).velocity('fadeIn', { duration: 400 });
+  this.$('.landing-image').css('opacity', 0).velocity('fadeIn', { duration: 400 });
+
+  // animate text
+  function animateText () {
     $text.velocity('fadeOut', { duration: 500, complete: function () {
       $(this).text(questions[questionIndex]);
       $(this).velocity('fadeIn', { duration: 500 });
       questionIndex = questionIndex >= questions.length - 1 ? 0 : questionIndex + 1;
 
       function rn () {
-        return parseInt(Math.random()*899) + 100;
+        return parseInt(Math.random()*899, 10) + 100;
       }
 
       $({ pro: 100, con: 100 }).delay(250).animate({ pro: rn(), con: rn() }, {
@@ -26,8 +31,8 @@ Template.landing.rendered = function () {
     }});
   };
 
-  animate();
-  this._animateHandle = Meteor.setInterval(animate, 6000);
+  animateText();
+  this._animateHandle = Meteor.setInterval(animateText, 6000);
 };
 
 Template.landing.destroyed = function () {
