a2347065b7445167e1b976f5c5e202d0ebc8e2b2
Mark Lee <marklee.lhs@gmail.com>
2014-12-22 00:01:41 -0800

    Fix search input i18n

--- client/views/common/search.js
+++ client/views/common/search.js
@@ -14,7 +14,10 @@ var isSearching = function () {
 
 Template.searchInput.helpers({
   indexes: ['topics', 'users'],
-  searching: isSearching
+  searching: isSearching,
+  placeholder: function () {
+    return i18n.t('search_prompt');
+  }
 });
 
 Template.mainLayout.helpers({
@@ -36,7 +39,7 @@ Template.searchInput.events({
     searching.set(true);
   },
   'click #js-search-cancel': function (event, template) {
-    stopSearching(); 
+    stopSearching();
   }
 });
 
