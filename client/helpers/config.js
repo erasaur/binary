02fefddd5434cff2d1ff88c07fe38fa3dafa99f4
Mark Lee <marklee.lhs@gmail.com>
2014-12-24 20:05:01 -0800

    Configure growl notifications

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -6,3 +6,8 @@ Avatar.options = {
 
 Herald.settings.queueTimer = 5000; // 86400000; // once a day
 Herald.settings.useIronRouter = false;
+
+toastr.options.timeOut = 5000;
+toastr.options.showDuration = 300;
+toastr.options.hideDuration = 300;
+toastr.options.positionClass = 'toast-top-center';
