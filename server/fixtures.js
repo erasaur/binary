a11fa7b5f53736c0fcc060050bb5cab70df0c351
Mark Lee <marklee.lhs@gmail.com>
2014-12-26 22:10:37 -0800

    Setup config for deploying with meteor up

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,10 +1,12 @@
-if (Meteor.users.find().count() === 0) {
-  Accounts.createUser({
-    email: 'test@example.com',
-    password: 'asdf123',
-    profile: {
-      name: 'Test User',
-      bio: i18n.t('default_profile')
-    }
-  });
-}
+// Meteor.startup(function () {
+//   if (Meteor.users.find().count() === 0) {
+//     Accounts.createUser({
+//       email: 'test@example.com',
+//       password: 'asdf123',
+//       profile: {
+//         name: 'test user',
+//         bio: 'Teset User is here'
+//       }
+//     });
+//   }
+// });
