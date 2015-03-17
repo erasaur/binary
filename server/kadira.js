04ffb49023f2f097b325536d87fb950b37b54f47
Mark Lee <marklee.lhs@gmail.com>
2015-03-16 22:13:11 -0700

    Move kadira keys to settings file

--- server/kadira.js
+++ server/kadira.js
@@ -1,3 +1,3 @@
 Meteor.startup(function () {
-  Kadira.connect('XQQfFaD3MckQS2adc', '5b1679c8-4ad6-4810-803c-555081b652bd')
+  Kadira.connect(Meteor.settings.kadiraId, Meteor.settings.kadiraSecret);
 });
