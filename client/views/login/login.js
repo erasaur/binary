6233742c67d8a147f17e07b14a259fdb4f4f1de0
erasaur <erasaur@gmail.com>
2015-04-28 15:08:16 -0700

    Swap out landing form errors #12, move login form into a template so we can reuse it in the signup modal

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -1,27 +1,17 @@
-Template.login.events({
-	'input input': function (event, template) {
-		fadeElement(template.$('.landing-form-errors'));
-	},
+Template.loginForm.events({
 	'submit #js-login-form': function (event, template) {
 		event.preventDefault();
 		var email = template.find('#js-email').value;
 		var	password = template.find('#js-password').value;
-		var $errors = template.$('.landing-form-errors');
-
-		function showError (message) {
-			$errors
-				.html('<li>' + message + '</li>')
-				.velocity('fadeIn', { duration: 1000 });
-		}
 
 		if (!email || !password) {
-			showError(i18n.t('missing_fields'));
+			toastr.warning(i18n.t('missing_fields'));
 			return;
 		}
 
 		Meteor.loginWithPassword(email, password, function (error) {
 			if (error) {
-				showError(i18n.t('login_error'));
+				toastr.warning(i18n.t('login_error'));
 			}
 		});
 	}
